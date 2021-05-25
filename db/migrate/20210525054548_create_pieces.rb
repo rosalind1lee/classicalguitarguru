dff98c4adc69c793aa1505605f6ce6847bf7eece
rosalind1lee <rlee90@chicagobooth.edu>
2021-05-25 05:46:29 +0000

    generate piece

---
+++ db/migrate/20210525054548_create_pieces.rb
@@ -0,0 +1,14 @@
+class CreatePieces < ActiveRecord::Migration[6.0]
+  def change
+    create_table :pieces do |t|
+      t.string :title
+      t.integer :composer_id
+      t.integer :arranger_id
+      t.integer :ratings_count
+      t.integer :favorites_count
+      t.integer :comments_count
+
+      t.timestamps
+    end
+  end
+end
