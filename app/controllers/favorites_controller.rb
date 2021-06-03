f8aeaab08d7188024fe50c1461e720e711c301b7
rosalind1lee <rlee90@chicagobooth.edu>
2021-06-03 04:44:50 +0000

    made datalist in favorites index

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -38,21 +38,33 @@ class FavoritesController < ApplicationController
   def create
     
     the_piece_id = params.fetch("query_piece_id")
-    the_user_id = @current_user.id
-    fav = Favorite.where({ :user_id => the_user_id }).where({ :piece_id => the_piece_id}).at(0)
+    piece_title = the_piece_id.split("-")[0].strip
+    composer_name = the_piece_id.split("-")[1].strip
+    p piece_title
+    p composer_name
 
-    if fav == nil
-      the_favorite = Favorite.new
-      the_favorite.user_id = the_user_id
-      the_favorite.piece_id = the_piece_id
-      if the_favorite.valid?
-        the_favorite.save
-        redirect_to("/favorites", { :notice => "Favorite created successfully." })
-      else
-        redirect_to("/favorites", { :alert => "Favorite failed to create successfully." })
-      end
+    the_composer = Composer.where({ :name => composer_name }).at(0)
+    the_piece = Piece.where({ :title => piece_title }).where({ :composer_id => the_composer.id }).at(0)
+    the_user_id = @current_user.id
+
+    if the_piece == nil
+      redirect_to("/favorites", { :alert => "Piece has not been added to our collection yet." })
     else
-      redirect_to("/favorites", { :alert => "Piece is already in your favorites." })
+      fav = Favorite.where({ :user_id => the_user_id }).where({ :piece_id => the_piece.id}).at(0)
+
+      if fav == nil
+        the_favorite = Favorite.new
+        the_favorite.user_id = the_user_id
+        the_favorite.piece_id = the_piece.id
+        if the_favorite.valid?
+          the_favorite.save
+          redirect_to("/favorites", { :notice => "Favorite created successfully." })
+        else
+          redirect_to("/favorites", { :alert => "Favorite failed to create successfully." })
+        end
+      else
+        redirect_to("/favorites", { :alert => "Piece is already in your favorites." })
+      end
     end
   end
 
